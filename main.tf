30bb0a27a21dbb7f45df624a9f5ef176945c7587
German Gonzalez <german.gonzalez.ext@adevinta.com>
2025-02-23 17:04:33 +0100

    chore(terraform): refactor in main to separate provider and user variables

--- main.tf
+++ main.tf
@@ -1,23 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.40.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "eu-central-1"
-}
-
 resource "aws_vpc" "main" {
-  cidr_block = "192.168.1.0/25"
-  enable_dns_support = "true"
-  enable_dns_hostnames = "true"
-  instance_tenancy = "default"
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+  instance_tenancy     = "default"
 
-    tags = {
-        Name = "GG"
-    }
+  tags = {
+    Name = "${var.prefix}-vpc"
+  }
 }
