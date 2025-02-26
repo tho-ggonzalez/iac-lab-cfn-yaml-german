85438f116357d50ab977bc1a1b9e473526120fe5
German Gonzalez <german.gonzalez.ext@adevinta.com>
2025-02-26 17:59:57 +0100

    feat(terraform): create session2 folder

--- session2/network.tf
+++ session2/network.tf
@@ -139,3 +139,32 @@ resource "aws_route_table_association" "private_routetable_assoc_2" {
   subnet_id      = aws_subnet.private_subnet_2.id
   route_table_id = aws_route_table.private_routetable.id
 }
+
+resource "aws_flow_log" "main_vpc_flow_log" {
+  vpc_id          = aws_vpc.main_vpc.id
+  log_destination = aws_cloudwatch_log_group.vpc_flow_logs.arn
+  traffic_type    = "ALL"
+
+  tags = {
+    Name = "${var.prefix}-vpc-flow-log"
+  }
+}
+
+resource "aws_kms_key" "cloudwatch_log_key" {
+  description             = "KMS key for encrypting VPC Flow Logs"
+  deletion_window_in_days = 7
+  enable_key_rotation     = true
+
+  tags = {
+    Name = "${var.prefix}-cloudwatch-kms-key"
+  }
+}
+
+resource "aws_cloudwatch_log_group" "vpc_flow_logs" {
+  name              = "/aws/vpc/flow-logs/${aws_vpc.main_vpc.id}"
+  kms_key_id        = aws_kms_key.cloudwatch_log_key.arn
+
+  tags = {
+    Name = "${var.prefix}-vpc-flow-log-group"
+  }
+}
