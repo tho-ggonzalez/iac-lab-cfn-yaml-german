48fa32342c1183760e7ef8dc3f67564cf935bb24
German Gonzalez <german.g@thoughtworks.com>
2025-04-09 20:32:19 +0200

    chore(terraform): update db secret arn

--- session_6/locals.tf
+++ session_6/locals.tf
@@ -3,6 +3,6 @@ locals {
   number_of_private_subnets = 2
   number_of_secure_subnets  = 2
 
-  db_secret_arn    = "arn:aws:secretsmanager:eu-central-1:160071257600:secret:dev/dbgg-EuMWhn"
+  db_secret_arn    = "arn:aws:secretsmanager:eu-central-1:160071257600:secret:dev/dbgg-yTIiSu"
   db_secret_key_id = "aws/secretsmanager"
 }
